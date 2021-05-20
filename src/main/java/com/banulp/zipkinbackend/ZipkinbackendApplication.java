f2da2fe3087284bf986b4902280eb444936b5961
박경옥(Park-KyungOk)(banulp) <banulp@ncsoft.com>
2021-05-20 17:42:22 +0900

    dp

--- src/main/java/com/banulp/zipkinbackend/ZipkinbackendApplication.java
+++ src/main/java/com/banulp/zipkinbackend/ZipkinbackendApplication.java
@@ -1,30 +1,18 @@
 package com.banulp.zipkinbackend;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@RestController
-//@EnableAutoConfiguration
 public class ZipkinbackendApplication {
 
-//	private static final Logger log = LoggerFactory.getLogger(ZipkinbackendApplication.class);
-	//	@Value("${spring.sleuth.enabled}")
-//	@Value("${spring.zipkin.baseUrl}")
-//	String se;
-//
-//	@RequestMapping("/")
-//	String home() {
-//		log.info("Hello world!" + se);
-//		return "Hello World!";
-//	}
+	//	https://cloud.spring.io/spring-cloud-sleuth/2.0.x/multi/multi__sampling.html
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkinbackendApplication.class, args);
